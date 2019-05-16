bc61f2a46577a8bcc5213a0812662087c8aa5e4d
computerphilosopher <abc3242@gmail.com>
2019-05-16 19:08:01 +0900

    모델 구현

--- todo_manager/views.py
+++ todo_manager/views.py
@@ -1,6 +1,10 @@
+"""views.py """
+
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 
 def todo_list(request):
-    return render(request, 'todo_manager/todo_list.html', {})
+    tasks = Task.objects.all()
+    return render(request, 'todo_manager/todo_list.html', {'tasks':tasks})
