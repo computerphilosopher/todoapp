2caaa4bac69a5b75726c491832bd2e7e11f09791
computerphilosopher <abc3242@gmail.com>
2019-05-18 14:30:14 +0900

    상세 페이지 만듦

--- todo_manager/urls.py
+++ todo_manager/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
+    path('update', views.update, name='update'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
--- todo_manager/views.py
+++ todo_manager/views.py
@@ -2,9 +2,26 @@
 
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+def update(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return todo_list(request)
+    else:
+        form = TaskForm()
+    return render(request, 'todo_manager/update.html', {'form':form})
+
 
 def todo_list(request):
     tasks = Task.objects.all()
     return render(request, 'todo_manager/todo_list.html', {'tasks':tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'todo_manager/task_detail.html', {'task':task})
